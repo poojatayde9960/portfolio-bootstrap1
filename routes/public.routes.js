a35d4f66162f9cb86100c794edf1bb0e975df7e2
pooja <https://github.com/poojatayde9960/ecom.git>
2024-10-04 13:32:15 +0530

    ratelimit

--- routes/public.routes.js
+++ routes/public.routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router()
 // const Enquery = require("../models/Enquery")
+const { rateLimit } = require("express-rate-limit")
 const public = require("./../controllers/public.controller")
 
 
@@ -9,7 +10,7 @@ router
     .get("/get-project-details/:id", public.getProjectDetails)
 
     .get("/get-enquiry", public.getEnquery)
-    .post("/add-enquiry", public.addEnquiry)
+    .post("/add-enquiry", rateLimit({ windowMs: 1 * 60 * 1000, limit: 1 }), public.addEnquiry)
     .put("/update-enquiry/:id", public.updateEnquery)
     .delete("/delete-enquiry/:id", public.deleteEnquery)
 module.exports = router
